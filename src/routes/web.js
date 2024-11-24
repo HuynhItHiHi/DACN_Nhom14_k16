c9ac3813af8f930a466af6cbac1bfad288e7ebb4
Trung Huynh <trunghuynh68hp@gmail.com>
2024-11-24 10:42:03 +0700

    cập nhật

--- src/routes/web.js
+++
@@ -1,8 +0,0 @@
-const express= require('express')
-const router=express.Router();
-const {getHomePage,getImage} = require('../controllers/homeController')
-
-router.get('/',getHomePage)
-router.get('/image',getImage)
-
-module.exports = router
