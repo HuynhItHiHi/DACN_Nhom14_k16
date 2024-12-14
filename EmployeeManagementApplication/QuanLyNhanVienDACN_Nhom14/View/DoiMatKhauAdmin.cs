af6e1a92e98e43df650ff1eadf82d21d49f552de
Trung Huynh <trunghuynh68hp@gmail.com>
2024-12-15 01:01:22 +0700

    Cap Nhat

---
+++ EmployeeManagementApplication/QuanLyNhanVienDACN_Nhom14/View/DoiMatKhauAdmin.cs
@@ -0,0 +1,38 @@
+﻿using QuanLyNhanVienDACN_Nhom14.Control;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyNhanVienDACN_Nhom14.View
+{
+    public partial class DoiMatKhauAdmin : Form
+    {
+        public DoiMatKhauAdmin()
+        {
+            InitializeComponent();
+        }
+
+        private void confirm_Click(object sender, EventArgs e)
+        {
+            string currentPassword = currentPasswordTextBox.Text.ToString();
+            string newPassword = newPasswordTextBox.Text.ToString();
+            string newPasswordRetype = enterAgainPassword.Text.ToString();
+            if (currentPassword != null && newPassword != null && newPasswordRetype != null)
+            {
+                ChangePasswordAdminControl changePasswordControl = new ChangePasswordAdminControl();
+                changePasswordControl.ChangePassword(currentPassword, newPassword, newPasswordRetype);
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Hãy điền đầy đủ thông tin.");
+            }
+        }
+    }
+}
